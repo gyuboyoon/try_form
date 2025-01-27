29193d2ba2e45ea1af41d27b738a50a47881124e
gyubo <yoon9yubo@gmail.com>
2025-01-27 21:24:57 +0900

    validation timing

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,11 +6,14 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid = function handleSubmit(event) {
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
+  function handleSubmit(event) {
     event.preventDefault();
 
     console.log(enteredValues);
-  };
+  }
 
   function handleInputChage(identifier, value) {
     setEnteredValues((prevValues) => ({
@@ -19,28 +22,44 @@ export default function Login() {
     }));
   }
 
-  return;
-  <form onSubmit={handleSubmit}>
-    <h2>Login</h2>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
 
-    <div className="control-row">
-      <div className="control no-margin">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={(event) => handleInputChage("email", event.target.value)}
-          value={enteredValues.email}
-        />
-      </div>
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={(event) => handleInputChage("email", event.target.value)}
+            value={enteredValues.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>이메일 주소를 입력해주세요!</p>}
+          </div>
+        </div>
 
-      <div className="control no-margin">
-        <label htmlFor="password">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" ref={password} />
-        </label>
+        <div className="control no-margin">
+          <label htmlFor="password">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              onChange={(event) =>
+                handleInputChage("password", event.target.value)
+              }
+              //   ref={password}
+            />
+          </label>
+        </div>
       </div>
-    </div>
-  </form>;
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
 }
